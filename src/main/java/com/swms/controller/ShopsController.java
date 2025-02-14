be25d7960419fd793901faaafd3d94aea178dd03
Amrit9472 <amr.raj112@gmail.com>
2025-02-14 17:59:24 +0530

    shop response change

--- src/main/java/com/swms/controller/ShopsController.java
+++ src/main/java/com/swms/controller/ShopsController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/shop")
 public class ShopsController {
 
@@ -52,15 +54,20 @@ public class ShopsController {
 	}
 
 	@GetMapping("/checkShopId/{gstId}")
-	public ResponseEntity<Map<String, Object>> checkShopDetailsPresentDatabases(@PathVariable String gstId) {
+	public ResponseEntity<?> checkShopDetailsPresentDatabases(@PathVariable String gstId) {
 		Shops shops = shopServiceImpl.checkShopDetailsPresentInDb(gstId);
 		Map<String, Object> response = new HashMap<>();
 		if (shops != null) {
 			response.put("exists", true);
 			response.put("shopId", shops.getId());
+			response.put("shopGstId",shops.getShopGstId());
+			response.put("shopName", shops.getShop_name());
+			response.put("contactInfo",shops.getContact_info());
+			response.put("type",shops.getType());
 			return ResponseEntity.ok(response);
 		} else {
 			response.put("exists", false);
+			response.put("message", "Shop not found");
 			return ResponseEntity.ok(response);
 		}
 
